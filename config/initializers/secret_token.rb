c718da71eafb00a5607a46c42e39de9bf350c43a
Yusuf <yusuf.saber@hotmail.com>
2011-12-14 02:17:45 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trustious::Application.config.secret_token = '48c5669fc698748c25a9e1bc23aea15f3f4f218cdb57674c3026ceaca3f21413142e05171e4102b5a92e8a6a0a3edaf976b6b50c45923bde1825e46d3908b22f'
